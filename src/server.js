6d4721c995d873afe341d07c2832c4f4d13d70d8
Tadeo <tadeofrr13@gmail.com>
2026-07-03 17:54:32 -0300

    chore: project skeleton with Express, Mongoose, CORS, Morgan

---
+++ src/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const { PORT = 3000, MONGO_URI } = process.env;
+
+if (!MONGO_URI) {
+  console.error('MONGO_URI is required');
+  process.exit(1);
+}
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
